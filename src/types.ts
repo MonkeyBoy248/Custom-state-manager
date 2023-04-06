ff58af737310dce86ec06d55752d302122a335d3
Kirill Temnyi <kirik201100@mail.ru>
2023-04-06 18:06:40 +0300

    feat: update types

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,13 @@ export interface Subscribable<T> {
   subscribe: (subscriber: Subscriber<T>) => () => void;
   publish: (value: T) => void;
 }
+
+export type StateSlice<State> =
+  | Partial<State>
+  | ((state: Partial<State>) => Partial<State>);
+
+export interface Store<State> {
+  getState: () => State;
+  subscribe: (subscriber: Subscriber<State>) => void;
+  updateState: (stateSlice: StateSlice<State>) => void;
+}
