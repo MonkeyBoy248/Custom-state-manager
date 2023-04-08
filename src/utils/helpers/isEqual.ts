5079cea4f2665f0ab19f207aba522fe24d3b2479
Kirill Temnyi <kirik201100@mail.ru>
2023-04-08 21:25:47 +0300

    feat: create a function for shallow comparison

---
+++ src/utils/helpers/isEqual.ts
@@ -0,0 +1,69 @@
+export function isEqual<T>(firstValue: T, secondValue: T): boolean {
+  if (Object.is(firstValue, secondValue)) {
+    return true;
+  }
+
+  if (
+    typeof firstValue !== 'object' ||
+    firstValue === null ||
+    typeof secondValue !== 'object' ||
+    secondValue === null
+  ) {
+    return false;
+  }
+
+  if (firstValue instanceof Map && secondValue instanceof Map) {
+    if (firstValue.size !== secondValue.size) {
+      return false;
+    }
+
+    for (const [key, value] of firstValue) {
+      if (!Object.is(value, secondValue.get(key))) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  if (firstValue instanceof Set && secondValue instanceof Set) {
+    if (firstValue.size !== secondValue.size) {
+      return false;
+    }
+
+    for (const value of firstValue) {
+      if (secondValue.has(value)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  const [firstValueKeys, secondValueKeys] = [
+    Object.keys(firstValue),
+    Object.keys(secondValue),
+  ];
+
+  if (firstValueKeys.length !== secondValueKeys.length) {
+    return false;
+  }
+
+  for (const key of firstValueKeys) {
+    if (!Object.hasOwn(secondValue, key)) {
+      return false;
+    }
+
+    if (!Object.is(firstValue[key as keyof T], secondValue[key as keyof T])) {
+      return false;
+    }
+
+    return true;
+  }
+
+  return true;
+}
+
+const arr = [1, 2, 3, 4];
+
+console.log(isEqual(arr, [1, 2, 4, 5]));
