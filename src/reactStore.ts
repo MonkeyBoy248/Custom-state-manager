771b1fc44fd4883481ec39ed9469804aace384d3
Kirill Temnyi <kirik201100@mail.ru>
2023-04-08 20:54:24 +0300

    feat: create reactStore file that contains custom useStore hook and the 'create' function that allows to create a global store

---
+++ src/reactStore.ts
@@ -0,0 +1,26 @@
+import { Store, StateCreator, Selector, EqualityFunction } from './types';
+import { createStore } from './baseStore';
+import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/shim/with-selector';
+
+export function useStore<Snapshot, Slice>(
+  store: Store<Snapshot>,
+  selector: Selector<Snapshot, Slice> = store.getState() as any,
+  equalityFunction?: EqualityFunction<Slice>
+): Slice {
+  return useSyncExternalStoreWithSelector(
+    store.subscribe,
+    store.getState,
+    store.getState,
+    selector,
+    equalityFunction
+  );
+}
+
+export function create<State>(stateCreator: StateCreator<State>) {
+  const store = createStore(stateCreator);
+
+  const useStoreWrapper = <Slice>(selector?: Selector<State, Slice>, equalityFunction?: EqualityFunction<Slice>) =>
+    useStore<State, Slice>(store, selector, equalityFunction);
+
+  return useStoreWrapper;
+}
