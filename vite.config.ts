52a6d74ff3b88ed02f08dad65eaf3987c60cdd36
Kirill Temnyi <kirik201100@mail.ru>
2023-04-10 17:42:42 +0300

    build: update config files for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths';
+import typescript from 'rollup-plugin-typescript2';
+import * as packageJSON from './package.json';
+import path from 'path'
+
+const resolvePaths = (...paths: string[]): string => {
+  return path.resolve(__dirname, ...paths)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolvePaths('./src/index.ts'),
+      name: 'SimpleStore',
+    },
+    rollupOptions: {
+      external: Object.keys(packageJSON.peerDependencies)
+    }
+  },
+  plugins: [react(), tsConfigPaths(), typescript({
+    tsconfig: resolvePaths('./tsconfig.json')
+  })],
 })
